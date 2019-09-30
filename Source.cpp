5cfcbfe9194a353bfe7033b75096c4970652597b
SStewart02 <55989448+SStewart02@users.noreply.github.com>
2019-09-30 15:51:43 +0100

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+
+ cout << "Hello, programming" << endl;
+ cout << endl;
+ cout << "Here we go!" << endl;
+
+	return 0;
+}
+
+// can eliminate vectors with pointers just need to know size;
+// can def max child size
